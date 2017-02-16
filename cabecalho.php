3ef383082e825d9488a7e80861905bf0e16e5792
RodrigoBaltazar <rlbaltazar2012@gmail.com>
2017-02-16 14:45:07 -0200

    html_cabecalho

---
+++ cabecalho.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <title>Milhagem Farmais</title>
+    <meta charset="utf-8">
+    <link href="css/bootstrap.css" rel="stylesheet" />
+    <link href="css/loja.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <a href="index.php" class="navbar-brand">Milhagem Farmais</a>
+            </div>
+            <div>
+                <ul class="nav navbar-nav">
+                    <li><a href="GERAR_EXTRATO_PHP_FAZER_ESSE_ARQUIVO.php">Extrato</a></li>
+                    <li><a href="GERAR_CADASTRO_PHP_FAZER_ESSE_ARQUIVO.php">Cadastrar</a></li>
+                    <li><a href="GERAR_SIMULADOR_PHP_FAZER_ESSE_ARQUIVO.php">Simulador</a></li>
+                    <li><a href="GERAR_ANIVERSARIANTES_MES_PHP_FAZER_ESSE_ARQUIVO.php">Aniversariantes do Mês</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+
+    <div class="principal">
