50512d19c248cc11d29f379aa856e0b71186ff5e
RodrigoBaltazar <rlbaltazar2012@gmail.com>
2017-02-21 15:09:45 -0300

    extrato

--- cliente.php
+++ cliente.php
@@ -12,3 +12,13 @@
 		VALUES ('{$id}', '{$nome}', '{$cpf}', '{$endereco}', '{$complemento}', '{$bairro}', '{$cep}', '{$cidade}', '{$telefone}', '{$celular}', '{$saldo}','{$email}')";
 		return mysqli_query($conexao, $query);
 	}
+
+	function listaCliente($conexao){
+		$clientes = array();
+		$resultado = mysqli_query($conexao, "select * FROM cliente");
+
+		while($cliente = mysqli_fetch_assoc($resultado)){
+			array_push($clientes, $cliente);
+		}
+		return $clientes;
+	}
---
+++ extrato-cliente.php
@@ -0,0 +1,16 @@
+<?php
+
+	include("cabecalho.php");
+	include("conexao.php");
+  include("cliente.php"):
+
+?>
+
+
+
+
+
+
+<?php
+  include("rodape.php");
+ ?>
