432be1acfb431094f66c706ed40d01136836e0f2
RodrigoBaltazar <rlbaltazar2012@gmail.com>
2017-06-26 11:26:40 -0300

    cadastro form 2.0

--- gerenciador-milhas/resources/views/clientes/formulario.blade.php
+++ gerenciador-milhas/resources/views/clientes/formulario.blade.php
@@ -2,13 +2,14 @@
 
 @section('conteudo')
 
-<h2>Cadastro de Clientes</h2>
+
 
 <div class="container">
 		<div class="row centered-form">
 		<div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
 					<div class="panel-heading">
 							<div class="panel-body">
+                <h2>Cadastro de Clientes</h2>
 				<!-- jeito de fazer esse form pego de:
           https://bootsnipp.com/snippets/featured/registration-form
        -->
@@ -21,13 +22,12 @@
             <!-- Abaixo  id e saldo alinhados-->
             <div class="col-xs-2 col-sm-6 col-md-6">
                         <div class="form-group">
-								          Id:<input type="text" class="form-control" name="id" placeholder="Numero Id">
+								          <input type="text" class="form-control" name="id" placeholder="Numero Id">
                         </div>
             </div>
 
             <div class="col-xs-2 col-sm-6 col-md-6">
                         <div class="form-group">
-                          Saldo:
          								 <input type="text" class="form-control" name="saldo" placeholder="Saldo">
                         </div>
             </div>
@@ -36,7 +36,6 @@
         <div class="row">
             <div class="col-xs-6 col-sm-6 col-md-12">
                         <div class="form-group">
-                          Nome:
                           <input type="text" class="form-control" name="nome" placeholder="Nome">
                         </div>
             </div>
@@ -45,7 +44,6 @@
           <div class="row">
             <div class="col-xs-6 col-sm-6 col-md-12">
                         <div class="form-group">
-          								 CPF:
           								 <input type="text" class="form-control" name="cpf" placeholder="CPF">
                         </div>
             </div>
@@ -54,37 +52,30 @@
           <div class="row">
                  <div class="col-xs-6 col-sm-6 col-md-12">
                              <div class="form-group">
-								                       Endereço:
 								                       <input type="text" class="form-control"  name="endereco" placeholder="Endereço">
                               </div>
                 </div>
                   <div class="col-xs-6 col-sm-6 col-md-6">
                              <div class="form-group">
-								                       Complemento:
 								                       <input type="text" class="form-control"  name="complemento" placeholder="Complemento">
                             </div>
                   </div>
                  <div class="col-xs-6 col-sm-6 col-md-6">
                              <div class="form-group">
-								                       Bairro:
 								                       <input type="text" class="form-control"  name="bairro" placeholder="Bairro">
                             </div>
                 </div>
-
-
           </div>
 
           <!-- Abaixo cidade e cep alinhados-->
           <div class="row">
               <div class="col-xs-6 col-sm-6 col-md-6">
                 <div class="form-group">
-                              Cidade:
                               <input type="text" class="form-control" name="cidade" placeholder="Cidade">
                 </div>
               </div>
                  <div class="col-xs-6 col-sm-6 col-md-6">
                     <div class="form-group">
-                               CEP:
                                <input type="text" class="form-control"  name="cep" placeholder="Cep">
                     </div>
                 </div>
@@ -93,13 +84,11 @@
           <div class="row">
             <div class="col-xs-6 col-sm-6 col-md-6">
                      <div class="form-group">
-                								 Telefone:
                 								 <input type="text" class="form-control" name="telefone" placeholder="Telefone">
                     </div>
             </div>
             <div class="col-xs-6 col-sm-6 col-md-6">
                     <div class="form-group">
-								                 Celular:
 								                 <input type="text" class="form-control" name="celular" placeholder="Celular">
                     </div>
             </div>
@@ -108,7 +97,6 @@
           <div class="row">
                  <div class="col-xs-6 col-sm-6 col-md-12">
                      <div class="form-group">
-            								 E-mail:
             								 <input type="email" class="form-control" name="email" placeholder="E-mail">
                      </div>
                 </div>
@@ -117,7 +105,6 @@
         <div class="row">
                <div class="col-xs-6 col-sm-6 col-md-6">
                    <div class="form-group">
-                             Data de Nascimento:
                              <input type="text" class="form-control" name="data_nascimento" placeholder="Data de Nascimento">
                    </div>
               </div>
--- gerenciador-milhas/storage/framework/views/7413b09ac3a773fb3dc2d4c29a77415973675555.php
+++ gerenciador-milhas/storage/framework/views/7413b09ac3a773fb3dc2d4c29a77415973675555.php
@@ -1,12 +1,13 @@
 <?php $__env->startSection('conteudo'); ?>
 
-<h2>Cadastro de Clientes</h2>
+
 
 <div class="container">
 		<div class="row centered-form">
 		<div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
 					<div class="panel-heading">
 							<div class="panel-body">
+                <h2>Cadastro de Clientes</h2>
 				<!-- jeito de fazer esse form pego de:
           https://bootsnipp.com/snippets/featured/registration-form
        -->
@@ -19,13 +20,12 @@
             <!-- Abaixo  id e saldo alinhados-->
             <div class="col-xs-2 col-sm-6 col-md-6">
                         <div class="form-group">
-								          Id:<input type="text" class="form-control" name="id" placeholder="Numero Id">
+								          <input type="text" class="form-control" name="id" placeholder="Numero Id">
                         </div>
             </div>
 
             <div class="col-xs-2 col-sm-6 col-md-6">
                         <div class="form-group">
-                          Saldo:
          								 <input type="text" class="form-control" name="saldo" placeholder="Saldo">
                         </div>
             </div>
@@ -34,7 +34,6 @@
         <div class="row">
             <div class="col-xs-6 col-sm-6 col-md-12">
                         <div class="form-group">
-                          Nome:
                           <input type="text" class="form-control" name="nome" placeholder="Nome">
                         </div>
             </div>
@@ -43,7 +42,6 @@
           <div class="row">
             <div class="col-xs-6 col-sm-6 col-md-12">
                         <div class="form-group">
-          								 CPF:
           								 <input type="text" class="form-control" name="cpf" placeholder="CPF">
                         </div>
             </div>
@@ -52,37 +50,30 @@
           <div class="row">
                  <div class="col-xs-6 col-sm-6 col-md-12">
                              <div class="form-group">
-								                       Endereço:
 								                       <input type="text" class="form-control"  name="endereco" placeholder="Endereço">
                               </div>
                 </div>
                   <div class="col-xs-6 col-sm-6 col-md-6">
                              <div class="form-group">
-								                       Complemento:
 								                       <input type="text" class="form-control"  name="complemento" placeholder="Complemento">
                             </div>
                   </div>
                  <div class="col-xs-6 col-sm-6 col-md-6">
                              <div class="form-group">
-								                       Bairro:
 								                       <input type="text" class="form-control"  name="bairro" placeholder="Bairro">
                             </div>
                 </div>
-
-
           </div>
 
           <!-- Abaixo cidade e cep alinhados-->
           <div class="row">
               <div class="col-xs-6 col-sm-6 col-md-6">
                 <div class="form-group">
-                              Cidade:
                               <input type="text" class="form-control" name="cidade" placeholder="Cidade">
                 </div>
               </div>
                  <div class="col-xs-6 col-sm-6 col-md-6">
                     <div class="form-group">
-                               CEP:
                                <input type="text" class="form-control"  name="cep" placeholder="Cep">
                     </div>
                 </div>
@@ -91,13 +82,11 @@
           <div class="row">
             <div class="col-xs-6 col-sm-6 col-md-6">
                      <div class="form-group">
-                								 Telefone:
                 								 <input type="text" class="form-control" name="telefone" placeholder="Telefone">
                     </div>
             </div>
             <div class="col-xs-6 col-sm-6 col-md-6">
                     <div class="form-group">
-								                 Celular:
 								                 <input type="text" class="form-control" name="celular" placeholder="Celular">
                     </div>
             </div>
@@ -106,7 +95,6 @@
           <div class="row">
                  <div class="col-xs-6 col-sm-6 col-md-12">
                      <div class="form-group">
-            								 E-mail:
             								 <input type="email" class="form-control" name="email" placeholder="E-mail">
                      </div>
                 </div>
@@ -115,7 +103,6 @@
         <div class="row">
                <div class="col-xs-6 col-sm-6 col-md-6">
                    <div class="form-group">
-                             Data de Nascimento:
                              <input type="text" class="form-control" name="data_nascimento" placeholder="Data de Nascimento">
                    </div>
               </div>
